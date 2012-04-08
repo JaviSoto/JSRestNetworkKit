45c2afd8884a5e264735d0439f40f3917912329f
Javier Soto <javier.api@gmail.com>
2012-04-08 15:09:52 -0700

    Initial Commit

---
+++ JSRestNetworkKitSampleProject/JSAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  JSAppDelegate.h
+//  JSRestNetworkKitSampleProject
+//
+//  Created by Javier Soto on 4/8/12.
+//  Copyright (c) 2012 MindSnacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ JSRestNetworkKitSampleProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JSRestNetworkKitSampleProject
+//
+//  Created by Javier Soto on 4/8/12.
+//  Copyright (c) 2012 MindSnacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JSAppDelegate class]));
+    }
+}
